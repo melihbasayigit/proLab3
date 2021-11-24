9793b2d03e858f269bd315e098cb2d96085d1a64
Melih Basayigit <melihbasayigit@gmail.com>
2021-11-24 23:29:52 +0300

    Create PlaneClass

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
+struct PlaneClass {
+    int priorityId;
+    int planeId;
+    int reqBoardTime; // Requested Boarding Time
+};
+
+typedef struct PlaneClass Plane;
+
+// GLOBAL VARIABLES
+int triedBoardCount = 0; // Tried Boarding Count. If this value reaches 2. 
+
+
 int main() {
-    
+    Plane airbus;
+    triedBoardCount++;
+    printf("%d",triedBoardCount);
 }
